6a46af3e88fe06e2306492e156f6f4a82f09c95c
UNICORN-67 <parthtsingh@gmail.com>
2025-03-28 11:55:16 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI || "mongodb+srv://parthtsingh:parthsingh@cluster0.a2hzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   OWNER_ID: process.env.OWNER_ID ? parseInt(process.env.OWNER_ID) : 7661577681,
   LOGGING_ENABLED: process.env.LOGGING_ENABLED === "true",
-  BOT_NAME: process.env.BOT_NAME || "TelegramGroupBot",
+  BOT_NAME: process.env.BOT_NAME || "GroupBot",
   API_ID: process.env.API_ID ? parseInt(process.env.API_ID) : 9683694,
   API_HASH: process.env.API_HASH || "c426d9f7087744afdafc961a620b6338",
 };
