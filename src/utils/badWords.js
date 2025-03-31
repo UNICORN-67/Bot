899f9f2af4820092dd03221b9da46f7d9fac51b9
UNICORN-67 <parthtsingh@gmail.com>
2025-03-31 12:08:41 +0530

    Create badWords.js

---
+++ src/utils/badWords.js
@@ -0,0 +1,32 @@
+module.exports = [
+    // 🔥 Common English Abuses
+    "fuck", "shit", "bastard", "asshole", "bitch", "dumbass", "motherfucker",
+    "pussy", "dickhead", "cock", "slut", "whore", "retard", "scumbag",
+
+    // 🔥 Hinglish Abuses (Updated with 50+ More)
+    "lodu", "chutiya", "bhosdike", "gandu", "madarchod", "behenchod",
+    "randi", "kutti", "chakka", "jhantu", "bhadwe", "suar", "haraamzada",
+    "mc", "bc", "madharchod", "bhenchod", "randwa", "kutte", "suar ke bacche",
+    "teri maa ki chut", "teri behan ki chut", "gand mara", "gand faad", "gaand masti",
+    "chodu", "lund", "lund le", "gand me le", "tatti", "nalle", "laude", "chikni",
+    "chutmar", "raand", "bhosdika", "chodna", "haraami", "kamine", "jhant", "jhantu",
+    "launde", "chodu saale", "bakchodi", "ullu ka pattha", "ullu", "bhosdi", "chut ke dhakkan",
+    "bhosdike madarchod", "behen ke laude", "randi ka bacha", "randi ka laal", "chutiye ke pille",
+    "maaderchod", "bhosda", "hijda", "chutiyapa", "gaand mein danda", "gand me ghus ja", "gaand masti",
+    "teri maa randi", "behen ka lauda", "ullu ke patthe", "teri maa ki chut me keeda", "tatti kha le",
+    "gaand fat gayi", "suar ke pille", "kamina kutta", "chut ki rani", "randi ka ladka", "tujhe kaata",
+    "maa chod", "baap chod", "behen chod", "chacha chod", "bhanja chod", "bhosda phat gaya",
+    "chut ka bhoot", "bhosdi ka bhikhari", "gand phat gayi", "bhenchod pagal", "lode jaisa",
+    "randiyon ka baap", "lund hilana", "lund ka keeda", "choos le lund", "lund chus le", "chod diya tujhe",
+    "ullu ka beta", "hijde ka lauda", "tera baap hijda", "gand mein le le", "madarchod ki aulad",
+    "chut ka virus", "bhen ka bhoot", "chodu ke aulaad", "randiyon ka malik", "bhikhari saala",
+    "madharchod ki pehli aulad", "chut ka bhoot", "gand mein dhamaka", "gand me aag", "phat gayi teri",
+    
+    // 🔥 Hindi (Devanagari)
+    "चुतिया", "भोसड़ी", "गांडू", "मादरचोद", "बहनचोद", "रंडी", "कुत्ती", "झाँटू",
+    "भड़वे", "हरामजादा", "साला", "कमीन", "नल्ला", "टट्टी", "लौंडा", "भसड", "साले",
+    "गांड", "लंड", "लंडू", "पजामा उठाके भाग", "ठरकी", "फट्टू", "चक्के", "हरामी",
+    "गटर का कीड़ा", "गंडू", "नालायक", "हरामी का पिल्ला", "झंड", "बेवड़ा", "गधा",
+    "लुच्चा", "लफंगा", "बंडल", "साला टट्टी", "तेरी बहन की गांड", "तेरी माँ की", 
+    "तेरी बहन की", "तेरी ग़ांड", "तेरी गांड मस्ती", "नंगा", "गन्दा आदमी"
+];
