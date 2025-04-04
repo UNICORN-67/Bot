5c59d9b62e908820f81af36f413bd4b3dd924c64
UNICORN-67 <parthtsingh@gmail.com>
2025-04-04 15:40:10 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -8,4 +8,11 @@ module.exports = {
   BOT_NAME: process.env.BOT_NAME || "GroupBot",
   API_ID: process.env.API_ID ? parseInt(process.env.API_ID) : 9683694,
   API_HASH: process.env.API_HASH || "c426d9f7087744afdafc961a620b6338",
+ LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID || '',
+  NSFW_DETECTION_ENABLED: process.env.NSFW_DETECTION_ENABLED === 'true',
+  SPAM_PROTECTION_ENABLED: process.env.SPAM_PROTECTION_ENABLED === 'true',
+  ABUSE_FILTER_ENABLED: process.env.ABUSE_FILTER_ENABLED === 'true',
+
+  TEMP_DIR: process.env.TEMP_DIR || './src/temp',
+
 };
